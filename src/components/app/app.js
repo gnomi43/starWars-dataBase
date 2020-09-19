e9e073918eec00d817a85e3c27a3f93920a18fe5
Nikita Baranov <khabnet47@mail.ru>
2020-09-19 18:36:58 +0300

    build app

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -23,7 +23,7 @@ export default class App extends React.Component {
     return (
       <ErrorBoundry>
         <SwapiServiceProvider value={this.swapiService} >
-          <Router>
+          <Router basename="/stardb">
             <div className="stardb-app">
               <Header />
               <RandomPlanet />
